7495c18132d6b5b5a51e7c1fb6e4a4c43d948598
Tharit Sinsunthorn <tharit@jaops.com>
2024-09-01 00:24:12 +0900

    add camera publisher ros2 script

---
+++ src/demo_foxglove/pub_camera.py
@@ -0,0 +1,287 @@
+import carb
+from isaacsim import SimulationApp
+import sys
+
+BACKGROUND_STAGE_PATH = "/background"
+BACKGROUND_USD_PATH = "/Isaac/Environments/Simple_Warehouse/warehouse_with_forklifts.usd"
+
+CONFIG = {"renderer": "RayTracedLighting", "headless": False}
+
+# Example ROS2 bridge sample demonstrating the manual loading of stages and manual publishing of images
+simulation_app = SimulationApp(CONFIG)
+import omni
+import numpy as np
+from omni.isaac import nucleus
+from omni.isaac.core import SimulationContext
+from omni.isaac.core.utils import stage, extensions
+import omni.graph.core as og
+import omni.replicator.core as rep
+import omni.syntheticdata._syntheticdata as sd
+
+from omni.isaac.core.utils.prims import set_targets
+from omni.isaac.sensor import Camera
+import omni.isaac.core.utils.numpy.rotations as rot_utils
+from omni.isaac.core.utils.prims import is_prim_path_valid
+from omni.isaac.core_nodes.scripts.utils import set_target_prims
+
+# Enable ROS2 bridge extension
+extensions.enable_extension("omni.isaac.ros2_bridge")
+
+simulation_app.update()
+
+simulation_context = SimulationContext(stage_units_in_meters=1.0)
+
+# Locate Isaac Sim assets folder to load environment and robot stages
+assets_root_path = nucleus.get_assets_root_path()
+if assets_root_path is None:
+    carb.log_error("Could not find Isaac Sim assets folder")
+    simulation_app.close()
+    sys.exit()
+
+# Loading the environment
+stage.add_reference_to_stage(assets_root_path + BACKGROUND_USD_PATH, BACKGROUND_STAGE_PATH)
+
+
+###### Camera helper functions for setting up publishers. ########
+
+# Paste functions from the tutorial here
+def publish_camera_tf(camera: Camera):
+    camera_prim = camera.prim_path
+
+    if not is_prim_path_valid(camera_prim):
+        raise ValueError(f"Camera path '{camera_prim}' is invalid.")
+
+    try:
+        # Generate the camera_frame_id. OmniActionGraph will use the last part of
+        # the full camera prim path as the frame name, so we will extract it here
+        # and use it for the pointcloud frame_id.
+        camera_frame_id=camera_prim.split("/")[-1]
+
+        # Generate an action graph associated with camera TF publishing.
+        ros_camera_graph_path = "/CameraTFActionGraph"
+
+        # If a camera graph is not found, create a new one.
+        if not is_prim_path_valid(ros_camera_graph_path):
+            (ros_camera_graph, _, _, _) = og.Controller.edit(
+                {
+                    "graph_path": ros_camera_graph_path,
+                    "evaluator_name": "execution",
+                    "pipeline_stage": og.GraphPipelineStage.GRAPH_PIPELINE_STAGE_SIMULATION,
+                },
+                {
+                    og.Controller.Keys.CREATE_NODES: [
+                        ("OnTick", "omni.graph.action.OnTick"),
+                        ("IsaacClock", "omni.isaac.core_nodes.IsaacReadSimulationTime"),
+                        ("RosPublisher", "omni.isaac.ros2_bridge.ROS2PublishClock"),
+                    ],
+                    og.Controller.Keys.CONNECT: [
+                        ("OnTick.outputs:tick", "RosPublisher.inputs:execIn"),
+                        ("IsaacClock.outputs:simulationTime", "RosPublisher.inputs:timeStamp"),
+                    ]
+                }
+            )
+
+        # Generate 2 nodes associated with each camera: TF from world to ROS camera convention, and world frame.
+        og.Controller.edit(
+            ros_camera_graph_path,
+            {
+                og.Controller.Keys.CREATE_NODES: [
+                    ("PublishTF_"+camera_frame_id, "omni.isaac.ros2_bridge.ROS2PublishTransformTree"),
+                    ("PublishRawTF_"+camera_frame_id+"_world", "omni.isaac.ros2_bridge.ROS2PublishRawTransformTree"),
+                ],
+                og.Controller.Keys.SET_VALUES: [
+                    ("PublishTF_"+camera_frame_id+".inputs:topicName", "/tf"),
+                    # Note if topic_name is changed to something else besides "/tf",
+                    # it will not be captured by the ROS tf broadcaster.
+                    ("PublishRawTF_"+camera_frame_id+"_world.inputs:topicName", "/tf"),
+                    ("PublishRawTF_"+camera_frame_id+"_world.inputs:parentFrameId", camera_frame_id),
+                    ("PublishRawTF_"+camera_frame_id+"_world.inputs:childFrameId", camera_frame_id+"_world"),
+                    # Static transform from ROS camera convention to world (+Z up, +X forward) convention:
+                    ("PublishRawTF_"+camera_frame_id+"_world.inputs:rotation", [0.5, -0.5, 0.5, 0.5]),
+                ],
+                og.Controller.Keys.CONNECT: [
+                    (ros_camera_graph_path+"/OnTick.outputs:tick",
+                        "PublishTF_"+camera_frame_id+".inputs:execIn"),
+                    (ros_camera_graph_path+"/OnTick.outputs:tick",
+                        "PublishRawTF_"+camera_frame_id+"_world.inputs:execIn"),
+                    (ros_camera_graph_path+"/IsaacClock.outputs:simulationTime",
+                        "PublishTF_"+camera_frame_id+".inputs:timeStamp"),
+                    (ros_camera_graph_path+"/IsaacClock.outputs:simulationTime",
+                        "PublishRawTF_"+camera_frame_id+"_world.inputs:timeStamp"),
+                ],
+            },
+        )
+    except Exception as e:
+        print(e)
+
+    # Add target prims for the USD pose. All other frames are static.
+    set_target_prims(
+        primPath=ros_camera_graph_path+"/PublishTF_"+camera_frame_id,
+        inputName="inputs:targetPrims",
+        targetPrimPaths=[camera_prim],
+    )
+    return
+
+def publish_camera_info(camera: Camera, freq):
+    from omni.isaac.ros2_bridge import read_camera_info
+    # The following code will link the camera's render product and publish the data to the specified topic name.
+    render_product = camera._render_product_path
+    step_size = int(60/freq)
+    topic_name = camera.name+"_camera_info"
+    queue_size = 1
+    node_namespace = ""
+    frame_id = camera.prim_path.split("/")[-1] # This matches what the TF tree is publishing.
+
+    writer = rep.writers.get("ROS2PublishCameraInfo")
+    camera_info = read_camera_info(render_product_path=render_product)
+    writer.initialize(
+        frameId=frame_id,
+        nodeNamespace=node_namespace,
+        queueSize=queue_size,
+        topicName=topic_name,
+        width=camera_info["width"],
+        height=camera_info["height"],
+        projectionType=camera_info["projectionType"],
+        k=camera_info["k"].reshape([1, 9]),
+        r=camera_info["r"].reshape([1, 9]),
+        p=camera_info["p"].reshape([1, 12]),
+        physicalDistortionModel=camera_info["physicalDistortionModel"],
+        physicalDistortionCoefficients=camera_info["physicalDistortionCoefficients"],
+    )
+    writer.attach([render_product])
+
+    gate_path = omni.syntheticdata.SyntheticData._get_node_path(
+        "PostProcessDispatch" + "IsaacSimulationGate", render_product
+    )
+
+    # Set step input of the Isaac Simulation Gate nodes upstream of ROS publishers to control their execution rate
+    og.Controller.attribute(gate_path + ".inputs:step").set(step_size)
+    return
+
+def publish_pointcloud_from_depth(camera: Camera, freq):
+    # The following code will link the camera's render product and publish the data to the specified topic name.
+    render_product = camera._render_product_path
+    step_size = int(60/freq)
+    topic_name = camera.name+"_pointcloud" # Set topic name to the camera's name
+    queue_size = 1
+    node_namespace = ""
+    frame_id = camera.prim_path.split("/")[-1] # This matches what the TF tree is publishing.
+
+    # Note, this pointcloud publisher will simply convert the Depth image to a pointcloud using the Camera intrinsics.
+    # This pointcloud generation method does not support semantic labelled objects.
+    rv = omni.syntheticdata.SyntheticData.convert_sensor_type_to_rendervar(
+        sd.SensorType.DistanceToImagePlane.name
+    )
+
+    writer = rep.writers.get(rv + "ROS2PublishPointCloud")
+    writer.initialize(
+        frameId=frame_id,
+        nodeNamespace=node_namespace,
+        queueSize=queue_size,
+        topicName=topic_name
+    )
+    writer.attach([render_product])
+
+    # Set step input of the Isaac Simulation Gate nodes upstream of ROS publishers to control their execution rate
+    gate_path = omni.syntheticdata.SyntheticData._get_node_path(
+        rv + "IsaacSimulationGate", render_product
+    )
+    og.Controller.attribute(gate_path + ".inputs:step").set(step_size)
+
+    return
+
+def publish_depth(camera: Camera, freq):
+    # The following code will link the camera's render product and publish the data to the specified topic name.
+    render_product = camera._render_product_path
+    step_size = int(60/freq)
+    topic_name = camera.name+"_depth"
+    queue_size = 1
+    node_namespace = ""
+    frame_id = camera.prim_path.split("/")[-1] # This matches what the TF tree is publishing.
+
+    rv = omni.syntheticdata.SyntheticData.convert_sensor_type_to_rendervar(
+                            sd.SensorType.DistanceToImagePlane.name
+                        )
+    writer = rep.writers.get(rv + "ROS2PublishImage")
+    writer.initialize(
+        frameId=frame_id,
+        nodeNamespace=node_namespace,
+        queueSize=queue_size,
+        topicName=topic_name
+    )
+    writer.attach([render_product])
+
+    # Set step input of the Isaac Simulation Gate nodes upstream of ROS publishers to control their execution rate
+    gate_path = omni.syntheticdata.SyntheticData._get_node_path(
+        rv + "IsaacSimulationGate", render_product
+    )
+    og.Controller.attribute(gate_path + ".inputs:step").set(step_size)
+
+    return
+
+
+def publish_rgb(camera: Camera, freq):
+    # The following code will link the camera's render product and publish the data to the specified topic name.
+    render_product = camera._render_product_path
+    step_size = int(60/freq)
+    topic_name = camera.name+"_rgb"
+    queue_size = 1
+    node_namespace = ""
+    frame_id = camera.prim_path.split("/")[-1] # This matches what the TF tree is publishing.
+
+    rv = omni.syntheticdata.SyntheticData.convert_sensor_type_to_rendervar(sd.SensorType.Rgb.name)
+    writer = rep.writers.get(rv + "ROS2PublishImage")
+    writer.initialize(
+        frameId=frame_id,
+        nodeNamespace=node_namespace,
+        queueSize=queue_size,
+        topicName=topic_name
+    )
+    writer.attach([render_product])
+
+    # Set step input of the Isaac Simulation Gate nodes upstream of ROS publishers to control their execution rate
+    gate_path = omni.syntheticdata.SyntheticData._get_node_path(
+        rv + "IsaacSimulationGate", render_product
+    )
+    og.Controller.attribute(gate_path + ".inputs:step").set(step_size)
+
+    return
+
+###################################################################
+
+# Create a Camera prim. Note that the Camera class takes the position and orientation in the world axes convention.
+camera = Camera(
+    prim_path="/World/floating_camera",
+    position=np.array([-3.11, -1.87, 1.0]),
+    frequency=20,
+    resolution=(256, 256),
+    orientation=rot_utils.euler_angles_to_quats(np.array([0, 0, 0]), degrees=True),
+)
+camera.initialize()
+
+simulation_app.update()
+camera.initialize()
+
+############### Calling Camera publishing functions ###############
+
+# Call the publishers.
+# Make sure you pasted in the helper functions above, and uncomment out the following lines before running.
+
+approx_freq = 30
+publish_camera_tf(camera)
+publish_camera_info(camera, approx_freq)
+publish_rgb(camera, approx_freq)
+publish_depth(camera, approx_freq)
+publish_pointcloud_from_depth(camera, approx_freq)
+
+####################################################################
+
+# Initialize physics
+simulation_context.initialize_physics()
+simulation_context.play()
+
+while simulation_app.is_running():
+    simulation_context.step(render=True)
+
+simulation_context.stop()
+simulation_app.close()
