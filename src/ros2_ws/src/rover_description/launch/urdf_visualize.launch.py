a9d74777631b67f9aba6399a5326a6b1656b9798
ThathariT <93856100+TharitSinsunthorn@users.noreply.github.com>
2024-11-12 11:45:26 +0900

    Dev/foxglove rerun integration (#1)
    * add camera publisher ros2 script
    
    * add omnigraph for control pragyaan via ros2
    
    * move rviz config file
    
    * add foxglove layout json
    
    * add imu to usd ros2 file
    
    * change root layer property (metre/unit)
    
    * ignore thumbnail
    
    * pragyaan urdf from usd to urdf converter
    
    * ignore colcon generated files
    
    * update urdf and ros2 visualization pkg
    
    * adding dummy of pragyaan to isaacsim
    
    * edit pragyaan dummy usd
    
    * fix TF of pragyaan components
    
    * import new pragyaan to lunar surface
    
    * add joint state publisher omninode
    
    * fix pcl tf publisher
    
    * add read me for robot visulization
    
    * add rerun code and update readme
    
    * add joint state to rerun
    
    * lunar surface demo without lander
    
    * update pragyaan version inside lunar_surface_demo_ros2
    
    * fix imu error
    
    * add depth encoding converter node
    
    * update foxglove panel config
    
    * add foxglove and rerun picture
    
    * fix pcl error
    
    * remove logger
    
    * add launch file for foxglove demo
    
    * update readme
    
    * launch file for lunar demo
    
    * add MIT license
    
    * edit license
    
    * fix launch folder missing in rover_camera pkg
    
    * edit readme for demo launch
    
    * tested and adjusted
    
    * update readme
    
    ---------
    
    Co-authored-by: Tharit Sinsunthorn <tharit@jaops.com>
    Co-authored-by: Louis Burtz <ljburtz@gmail.com>

---
+++ src/ros2_ws/src/rover_description/launch/urdf_visualize.launch.py
@@ -0,0 +1,88 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch.conditions import IfCondition
+import xacro
+
+# this is the function launch  system will look for
+def generate_launch_description():
+
+    ####### DATA INPUT ##########
+    urdf_file = 'pragyaan_rover.urdf'
+    package_description = "rover_description"
+
+    use_joint_state_gui = LaunchConfiguration('use_joint_state_gui')
+    urdf_file = LaunchConfiguration('urdf_file')
+    robot_desc_path = LaunchConfiguration('robot_desc_path')
+
+    gui_arg = DeclareLaunchArgument(
+        'use_joint_state_gui',
+        default_value='True'
+    )
+
+    urdf_file_arg = DeclareLaunchArgument(
+        'urdf_file',
+        default_value='pragyaan_rover.urdf'
+    )
+
+    robot_desc_path_arg = DeclareLaunchArgument(
+        'robot_desc_path',
+        default_value=[os.path.join(get_package_share_directory(package_description), 
+                                    'urdf'),
+                                    '/', 
+                                    urdf_file],
+        description='URDF file'
+    )
+
+    # Robot State Publisher
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher_node',
+        emulate_tty=True,
+        parameters=[{'use_sim_time': True, 'robot_description': Command(['xacro ', robot_desc_path])}],
+        # parameters=[robot_description],  ## for xacro
+        output="screen"
+    )
+
+    # Joint State Publisher
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen',
+        condition=IfCondition(use_joint_state_gui)
+    )
+
+    # RVIZ Configuration
+    rviz_config_dir = os.path.join(
+        get_package_share_directory(package_description), 
+        'rviz', 
+        'urdf_vis.rviz'
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        name='rviz_node',
+        parameters=[{'use_sim_time': True}],
+        arguments=['-d', rviz_config_dir]
+    )
+
+
+    # create and return launch description object
+    return LaunchDescription(
+        [           
+            gui_arg,
+            urdf_file_arg,
+            robot_desc_path_arg,
+
+            robot_state_publisher_node,
+            rviz_node,
+            joint_state_publisher_node
+        ]
+    )
